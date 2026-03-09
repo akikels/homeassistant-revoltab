6c5ae65cdbfa21dd64d116a26170db9f5a698772
akikels <anton@kikels.ch>
2026-03-09 14:36:53 +0100

    Update number.py

--- custom_components/revoltab/number.py
+++ custom_components/revoltab/number.py
@@ -11,18 +11,19 @@ class RevoltabIntensity(NumberEntity):
     def __init__(self, api, device):
         self._api = api
         self._device_id = device.get("deviceId", "revoltab_default")
-        self._attr_name = "Intensity" # Name innerhalb des Geräts
+        self._device_name = device.get("deviceName", "Schlafzimmer")
+        self._attr_name = "Intensity" 
         self._attr_unique_id = f"{self._device_id}_intensity"
         self._attr_native_min_value = 0
         self._attr_native_max_value = 100
         self._attr_native_step = 1
-        self._attr_native_value = device.get("intensity", 50)
+        self._attr_native_value = float(device.get("intensity", 0))
 
     @property
     def device_info(self):
         return {
             "identifiers": {(DOMAIN, self._device_id)},
-            "name": self._api_device_name, # Nutzt den Namen des Geräts
+            "name": self._device_name,
             "manufacturer": "Revoltab",
             "model": "HIDE",
         }
