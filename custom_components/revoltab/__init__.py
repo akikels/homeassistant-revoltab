50d1a2a4228fc4e1dbe1241338fcc9242e4291cf
akikels <anton@kikels.ch>
2026-03-09 14:56:14 +0100

    Update __init__.py

--- custom_components/revoltab/__init__.py
+++ custom_components/revoltab/__init__.py
@@ -6,14 +6,12 @@ from .api import RevoltabAPI
 from .const import DOMAIN, CONF_API_KEY
 
 _LOGGER = logging.getLogger(__name__)
-
 SCAN_INTERVAL = 10 
 
 async def async_setup_entry(hass: HomeAssistant, entry):
     api = RevoltabAPI(entry.data[CONF_API_KEY])
 
     async def async_update_data():
-        """Zentrale Datenabfrage vom Revoltab Server."""
         return await api.get_device_status()
 
     coordinator = DataUpdateCoordinator(
@@ -24,7 +22,6 @@ async def async_setup_entry(hass: HomeAssistant, entry):
         update_interval=timedelta(seconds=SCAN_INTERVAL),
     )
 
-    # Erster Abruf beim Start
     await coordinator.async_config_entry_first_refresh()
 
     hass.data.setdefault(DOMAIN, {})
@@ -33,11 +30,11 @@ async def async_setup_entry(hass: HomeAssistant, entry):
         "coordinator": coordinator,
     }
 
-    await hass.config_entries.async_forward_entry_setups(entry, ["switch", "number", "sensor"])
+    await hass.config_entries.async_forward_entry_setups(entry, ["switch", "number", "sensor", "binary_sensor"])
     return True
 
 async def async_unload_entry(hass, entry):
-    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, ["switch", "number", "sensor"])
+    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, ["switch", "number", "sensor", "binary_sensor"])
     if unload_ok:
         hass.data[DOMAIN].pop(entry.entry_id)
     return unload_ok
