02295c18255f885fe73d010909d8a9725d098622
akikels <anton@kikels.ch>
2026-03-09 13:46:19 +0100

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .const import DOMAIN
+
+async def async_setup_entry(hass, entry):
+    hass.data.setdefault(DOMAIN, {})
+    await hass.config_entries.async_forward_entry_setups(entry, ["switch"])
+    return True
+
+async def async_unload_entry(hass, entry):
+    return await hass.config_entries.async_forward_entry_unload(entry, "switch")
