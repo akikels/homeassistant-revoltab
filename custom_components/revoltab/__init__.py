9f20893fa6fd811e247f853e98e9dc638819d00a
akikels <anton@kikels.ch>
2026-03-09 14:29:53 +0100

    Update __init__.py

--- custom_components/revoltab/__init__.py
+++ custom_components/revoltab/__init__.py
@@ -2,7 +2,7 @@ from .const import DOMAIN
 
 async def async_setup_entry(hass, entry):
     hass.data.setdefault(DOMAIN, {})
-    await hass.config_entries.async_forward_entry_setups(entry, ["switch"])
+    await hass.config_entries.async_forward_entry_setups(entry, ["switch", "number"])
     return True
 
 async def async_unload_entry(hass, entry):
