6039c3d22e6bef1a37737f9c87df3703b03a7792
akikels <anton@kikels.ch>
2026-03-09 14:24:28 +0100

    Update switch.py

--- custom_components/revoltab/switch.py
+++ custom_components/revoltab/switch.py
@@ -3,32 +3,23 @@ from .api import RevoltabAPI
 from .const import DOMAIN, CONF_API_KEY
 
 async def async_setup_entry(hass, entry, async_add_entities):
-    # Hier wird der Key aus der Konfiguration geholt, den du im Fenster eingegeben hast
-    api_key = entry.data[CONF_API_KEY]
-    api = RevoltabAPI(api_key)
-    
+    api = RevoltabAPI(entry.data[CONF_API_KEY])
     devices = await api.get_devices()
-    entities = [RevoltabDevice(api, d) for d in devices]
-    async_add_entities(entities)
+    async_add_entities([RevoltabSwitch(api, d) for d in devices])
 
-class RevoltabDevice(SwitchEntity):
+class RevoltabSwitch(SwitchEntity):
     def __init__(self, api, device):
         self._api = api
-        self._device = device
-        self._attr_name = device.get("name", f"HIDE {device['id']}")
-        self._attr_unique_id = f"revoltab_{device['id']}"
-        self._is_on = False
-
-    @property
-    def is_on(self):
-        return self._is_on
+        self._attr_name = device.get("deviceName", "HIDE Device")
+        self._attr_unique_id = device.get("deviceId", "revoltab_default")
+        self._attr_is_on = device.get("isOn") == 1
 
     async def async_turn_on(self, **kwargs):
-        if await self._api.send_command(self._device["id"], "open"):
-            self._is_on = True
+        if await self._api.send_command("start"):
+            self._attr_is_on = True
             self.async_write_ha_state()
 
     async def async_turn_off(self, **kwargs):
-        if await self._api.send_command(self._device["id"], "close"):
-            self._is_on = False
+        if await self._api.send_command("stop"):
+            self._attr_is_on = False
             self.async_write_ha_state()
