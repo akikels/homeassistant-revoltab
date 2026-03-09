eaca4ecea372dc27a92e14db5185df47169e1dc2
akikels <anton@kikels.ch>
2026-03-09 14:43:42 +0100

    Update __init__.py

--- custom_components/revoltab/__init__.py
+++ custom_components/revoltab/__init__.py
@@ -1,9 +1,38 @@
-from .const import DOMAIN
+from datetime import timedelta
+import logging
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from .api import RevoltabAPI
+from .const import DOMAIN, CONF_API_KEY
+
+_LOGGER = logging.getLogger(__name__)
 
 async def async_setup_entry(hass, entry):
+    api = RevoltabAPI(entry.data[CONF_API_KEY])
+
+    async def async_update_data():
+        return await api.get_device_status()
+
+    coordinator = DataUpdateCoordinator(
+        hass,
+        _LOGGER,
+        name="revoltab_device",
+        update_method=async_update_data,
+        update_interval=timedelta(seconds=30),
+    )
+
+    await coordinator.async_config_entry_first_refresh()
+
     hass.data.setdefault(DOMAIN, {})
+    hass.data[DOMAIN][entry.entry_id] = {
+        "api": api,
+        "coordinator": coordinator,
+    }
+
     await hass.config_entries.async_forward_entry_setups(entry, ["switch", "number"])
     return True
 
 async def async_unload_entry(hass, entry):
-    return await hass.config_entries.async_forward_entry_unload(entry, ["switch", "number"])
+    unload_ok = await hass.config_entries.async_forward_entry_unload(entry, ["switch", "number"])
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+    return unload_ok
