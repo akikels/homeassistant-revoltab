db9e44d81dfb8ec5f97625c1d1cef8526624e903
akikels <anton@kikels.ch>
2026-03-09 14:28:58 +0100

    Added Intensity

---
+++ custom_components/revoltab/number.py
@@ -0,0 +1,23 @@
+from homeassistant.components.number import NumberEntity
+from .const import DOMAIN, CONF_API_KEY
+
+async def async_setup_entry(hass, entry, async_add_entities):
+    from .api import RevoltabAPI
+    api = RevoltabAPI(entry.data[CONF_API_KEY])
+    devices = await api.get_devices()
+    async_add_entities([RevoltabIntensity(api, d) for d in devices])
+
+class RevoltabIntensity(NumberEntity):
+    def __init__(self, api, device):
+        self._api = api
+        self._attr_name = f"{device.get('deviceName', 'HIDE')} Intensity"
+        self._attr_unique_id = f"{device.get('deviceId', 'revoltab')}_intensity"
+        self._attr_native_min_value = 0
+        self._attr_native_max_value = 100
+        self._attr_native_step = 1
+        self._attr_native_value = device.get("intensity", 50)
+
+    async def async_set_native_value(self, value: float) -> None:
+        if await self._api.set_intensity(int(value)):
+            self._attr_native_value = value
+            self.async_write_ha_state()
