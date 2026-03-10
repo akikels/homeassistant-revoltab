9e18afe27f550e62623889240775dfdc3b45d0c5
Anton Kikels <a.kikels@interum.li>
2026-03-10 12:56:10 +0100

    Error correction

--- custom_components/revoltab/config_flow.py
+++ custom_components/revoltab/config_flow.py
@@ -35,8 +35,6 @@ class RevoltabConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         return RevoltabOptionsFlowHandler(config_entry)
 
 class RevoltabOptionsFlowHandler(config_entries.OptionsFlow):
-    def __init__(self, config_entry):
-        self.config_entry = config_entry
 
     async def async_step_init(self, user_input=None):
         if user_input is not None:
