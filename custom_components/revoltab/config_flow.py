8950876b0870c0cf526ba3a94bcd0777d19da6d7
Anton Kikels <a.kikels@interum.li>
2026-03-10 12:48:34 +0100

    Error correction

--- custom_components/revoltab/config_flow.py
+++ custom_components/revoltab/config_flow.py
@@ -1,7 +1,7 @@
 import voluptuous as vol
+import aiohttp
 from homeassistant import config_entries
 from homeassistant.core import callback
-import aiohttp
 from .const import DOMAIN, CONF_API_KEY
 
 class RevoltabConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -20,7 +20,7 @@ class RevoltabConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                         if resp.status == 200:
                             return self.async_create_entry(title="Revoltab", data={CONF_API_KEY: token})
                         errors["base"] = "invalid_auth"
-                except:
+                except Exception:
                     errors["base"] = "cannot_connect"
 
         return self.async_show_form(
@@ -42,12 +42,14 @@ class RevoltabOptionsFlowHandler(config_entries.OptionsFlow):
         if user_input is not None:
             return self.async_create_entry(title="", data=user_input)
 
+        current_interval = self.config_entry.options.get("scan_interval", 5)
+
         return self.async_show_form(
             step_id="init",
             data_schema=vol.Schema({
                 vol.Optional(
                     "scan_interval",
-                    default=self.config_entry.options.get("scan_interval", 5),
+                    default=current_interval,
                 ): vol.All(vol.Coerce(int), vol.Range(min=5, max=3600)),
             }),
         )
